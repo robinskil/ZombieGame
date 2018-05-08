5b896432ae3e5d8c0c74ad234cd2aa87724b973e
robinskil <33688350+robinskil@users.noreply.github.com>
2018-05-08 10:19:28 +0200

    Working on bullet

--- UltraAmazingZombieGame/UltraAmazingZombieGame/Weapon/Bullet/Bullet.cs
+++ UltraAmazingZombieGame/UltraAmazingZombieGame/Weapon/Bullet/Bullet.cs
@@ -11,11 +11,29 @@ namespace UltraAmazingZombieGame.Weapon.Bullet
     public class Bullet{
         public Vector2 position {get;private set;}
         public Vector2 startPosition {get;private set;}
-        public Bullet(float x , float y){
-
+        public Vector2 mousePos {get;private set;}
+        public Bullet(float x , float y , Vector2 mousePos){
+            this.startPosition = new Vector2(x,y);
+            this.position = new Vector2(startPosition.X,startPosition.Y);
+            this.mousePos = mousePos;
+            CalculatePath();
         }
-        public Bullet(Vector2 pos){
+        public Bullet(Vector2 pos , Vector2 mousePos){
+            this.startPosition = pos;
+            this.position = pos;
+            this.mousePos = mousePos;
+            CalculatePath();
+        }
+
+        private void CalculatePath()
+        {
             
+        }
+        public void Move(){
+            
+        }
+        public void DrawBullet(SpriteBatch spriteBatch){
+            spriteBatch.FillRectangle(new RectangleF(this.position.X,this.position.Y,4,2),Color.Red);
         }
     }
 }
